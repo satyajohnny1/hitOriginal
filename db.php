0604c32bb89f8abc4f3eccc9fbb076bec25aa93c
Satya <smlcodes@gmail.com>
2021-05-31 15:39:13 +0530

    Hit - New Code Changes

--- db.php
+++ db.php
@@ -48,10 +48,11 @@ $dbname = "hit2";
 
 
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "hit2";
+$servername = "sql208.unaux.com";
+$username = "unaux_28553267";
+$password = "123abcABC@";
+$dbname = "unaux_28553267_javabo";
+
 
 
 // Create connection
